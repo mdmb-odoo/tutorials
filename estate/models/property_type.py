1f031c73533ec0afcf6fce812864979408b26018
mdmb-odoo <mdmb@odoo.com>
2024-09-12 15:18:38 +0400

    Add constraints

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -1,7 +1,26 @@
-from odoo import models, fields
-
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 class PropertyType(models.Model):
     _name = "property_type"
 
     name = fields.Char(string="Type",required=True)
-    
+
+    _sql_constraints = [
+        ('check_unique','UNIQUE(name)','Type must be unique')
+    ]
+
+
+    # @api.constrains('name')
+    # def check_unique(self):
+    #     self.env.cr.execute(" SELECT CASE " \
+    #                         "WHEN NOT EXISTS ( "\
+    #                                 "SELECT 1 "\
+    #                                 "FROM property_type "\
+    #                                 "WHERE LOWER(name) = LOWER(%s) "\
+    #                             ") THEN TRUE "\
+    #                             "ELSE FALSE "\
+    #                         "END AS name_does_not_exist;"\
+    #                         ,[self.name])
+    #     if not self.env.cr.fetchall()[0][0]:
+    #         raise ValidationError("Name already exists")
+        
