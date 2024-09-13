8ee17dc7e53eb823b0f3dc890aa1e8bb73a8c458
mdmb-odoo <mdmb@odoo.com>
2024-09-13 17:04:20 +0400

    Ch11 Add the sprinkles

--- estate/models/property_tag.py
+++ estate/models/property_tag.py
@@ -2,9 +2,10 @@ from odoo import models, fields
 
 class PropertyType(models.Model):
     _name = "property_tag"
+    _order = "name"
 
     name = fields.Char(string="Tag",required=True)
-
+    color = fields.Integer(string="Color")
     _sql_constraints = [
         ('check_unique','UNIQUE(name)','Tag must be unique')
     ]
