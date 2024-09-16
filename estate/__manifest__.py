a5447f14518d8ce791853e6d7a9e5eca34a51273
mdmb-odoo <mdmb@odoo.com>
2024-09-16 17:47:46 +0400

    Ch15 Initial commit

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -21,7 +21,7 @@
         'views/property_type_views.xml',
         'views/property_tag_views.xml',
         'views/property_offer_views.xml',
-        'views/property_salesmen_views.xml',
+        'views/res_users_views.xml',
         'views/estate_menus.xml'
         ]
 
