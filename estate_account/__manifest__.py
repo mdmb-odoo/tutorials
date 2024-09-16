c74c05adbef0d17f6994f575816b2e31390fa8f8
mdmb-odoo <mdmb@odoo.com>
2024-09-16 15:39:45 +0400

    Ch13 Create invoice using account app

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate Accounting",
+
+    'summary': """
+        A new real estate and accounting apps link for onboarding purpose.
+    """,
+
+    'author': "MDMB",
+    'website': "https://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tutorials/estate_account',
+    'depends': ['estate','account'],
+    'application': True
+    # 'data': [
+    #     'security/ir.model.access.csv',
+    #     'views/estate_property_views.xml',
+    #     'views/property_type_views.xml',
+    #     'views/property_tag_views.xml',
+    #     'views/property_offer_views.xml',
+    #     'views/property_salesmen_views.xml',
+    #     'views/estate_menus.xml'
+    #     ]
+
+}
